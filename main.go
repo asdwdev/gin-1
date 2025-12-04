367cec94e6d6f10621c013929f6ca05bf12a9fd5
asdwdev <asdwdev@gmail.com>
2025-12-04 22:21:03 +0700

    cara bikin project gin & menjankan server kosong

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
+// cara bikin project gin & menjalankan server kosong
 package main
 
 import "github.com/gin-gonic/gin"
 
 func main() {
-	// bikin router gin
-	r := gin.Default()
-
-	// route GET di path "/"
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello gin!",
-		})
-	})
-
-	// jalankan server di port 8080
-	r.Run() // default :8008
+	r := gin.Default() // bikin router
+	r.Run()            // nyalain server (default port 8080)
 }
