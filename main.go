277519b3686367eb59c3e308111cfb0f415313ef
asdwdev <asdwdev@gmail.com>
2025-12-08 23:05:12 +0700

    bagian 5 - post json (body request)

--- main.go
+++ main.go
@@ -1,22 +1,55 @@
-// latihan bagian 4
+// Bagian 5 — POST JSON (Body Request)
+// API modern sangat sering menerima JSON, misalnya:
+
+// login (username, password)
+
+// register
+
+// tambah produk
+
+// update profil
+
+// upload data form
+
+// Di Gin, cara ambil JSON:
+
+// var body struct {
+//     Username string `json:"username"`
+//     Password string `json:"password"`
+// }
+
+// if err := c.BindJSON(&body); err != nil {
+//     c.JSON(400, gin.H{"error": "json invalid"})
+//     return
+// }
+
+//	c.JSON(200, gin.H{
+//	    "username": body.Username,
+//	    "password": body.Password,
+//	})
 package main
 
 import "github.com/gin-gonic/gin"
 
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func main() {
 	r := gin.Default()
 
-	r.GET("/user/:nama", func(c *gin.Context) {
-		nama := c.Param("nama")
-		c.JSON(200, gin.H{
-			"nama": nama,
-		})
-	})
+	r.POST("/login", func(c *gin.Context) {
+		var req LoginRequest
 
-	r.GET("/produk/:id", func(c *gin.Context) {
-		id := c.Param("id")
+		if err := c.BindJSON(&req); err != nil {
+			c.JSON(400, gin.H{"error": "body tidak valid"})
+			return
+		}
+
 		c.JSON(200, gin.H{
-			"id": id,
+			"username": req.Username,
+			"status":   "login oke",
 		})
 	})
 
