b36c3e195db5b5cb183d3b977259ec65ba2c40ff
asdwdev <asdwdev@gmail.com>
2025-12-08 22:56:00 +0700

    bagian 4 - path parameter

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
+// Bagian 4 — Path Parameter
+
+// Contoh:
+// /user/10
+// /produk/12345
+// /blog/2024/01/10
+
+// Path param dipakai saat kamu ingin menangani:
+
+// /user/:id
+
+// /article/:slug
+
+// /order/:orderId
 package main
 
 import "github.com/gin-gonic/gin"
@@ -5,13 +19,24 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 
-	r.GET("/greet", func(c *gin.Context) {
-		nama := c.Query("nama")
-		umur := c.DefaultQuery("umur", "0")
+	// 1️⃣ Cara membuat path parameter
+	r.GET("/user/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		c.JSON(200, gin.H{
+			"user_id": id,
+		})
+	})
+
+	// 2️⃣ Bisa lebih dari satu parameter
+	r.GET("/blog/:tahun/:bulan/:tanggal", func(c *gin.Context) {
+		tahun := c.Param("tahun")
+		bulan := c.Param("bulan")
+		tgl := c.Param("tanggal")
 
 		c.JSON(200, gin.H{
-			"nama": nama,
-			"umur": umur,
+			"tahun":  tahun,
+			"bulan":  bulan,
+			"tangal": tgl,
 		})
 	})
 
