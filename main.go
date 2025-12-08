d3484ceb4c7b14bc22e84e563e40f678702de79d
asdwdev <asdwdev@gmail.com>
2025-12-08 22:32:46 +0700

    bagian 2 - route dasar (get & post)

--- main.go
+++ main.go
@@ -1,14 +1,48 @@
+// 📌 Bagian 2 — Routing Dasar (GET & POST)
+
+// Routing = cara server menentukan “kalau user akses URL ini, jalankan handler ini.”
+// ✔ Routing metode HTTP:
+
+// GET
+
+// POST
+
+// PUT
+
+// DELETE
+
+// ✔ Routing dengan parameter:
+
+// Query param
+
+// Path param
+
+// Tapi sekarang kita bahas GET dan POST dulu.
+
 package main
 
 import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+
+	// 	1️⃣ Routing GET
+
+	// GET dipakai untuk ambil data
+	r.GET("/halo", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "hello world dari gin",
+			"pesan": "ini endpoint GET",
 		})
 	})
 
-	r.Run() // default: localhost:8080
+	// 	2️⃣ Routing POST
+
+	// POST dipakai untuk mengirim data.
+	r.POST("/kirim", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status": "data diterima",
+		})
+	})
+
+	r.Run()
 }
