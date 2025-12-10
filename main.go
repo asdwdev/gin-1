9f64370291ebbe1f05ad36162a3ef85fcb305732
asdwdev <asdwdev@gmail.com>
2025-12-10 22:04:48 +0700

    latihan bagian 7

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
-// 3️⃣ Contoh Middleware Auth Paling Dasar
+// latihan bagian 7
 package main
 
 import "github.com/gin-gonic/gin"
 
-func AuthMiddleware() gin.HandlerFunc {
+func CheckApiKey() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		token := c.GetHeader("Authorization")
-		if token != "secret123" {
-			c.JSON(401, gin.H{"error": "unauhorized"})
+		token := c.GetHeader("X-API-KEY")
+		if token != "rahasia123" {
+			c.JSON(401, gin.H{"error": "API key salah"})
 			c.Abort()
 			return
 		}
+
 		c.Next()
 	}
 }
@@ -18,11 +19,11 @@ func AuthMiddleware() gin.HandlerFunc {
 func main() {
 	r := gin.Default()
 
-	private := r.Group("/private")
-	private.Use(AuthMiddleware())
+	admin := r.Group("/admin")
+	admin.Use(CheckApiKey())
 
-	private.GET("/dashboard", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "selamat datang"})
+	admin.GET("/info", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "akses diterima"})
 	})
 
 	r.Run()
