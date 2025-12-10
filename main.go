31ff9d6c13e170cad931991f0cc1c1a0cd5ff1ad
asdwdev <asdwdev@gmail.com>
2025-12-10 21:47:48 +0700

    latihan bagian 6

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
-// Bagian 6 — Validator (VERY IMPORTANT)
+// latihan 6
 package main
 
 import "github.com/gin-gonic/gin"
 
-type RegisterRequest struct {
+type ProductRequest struct {
 	Nama  string `json:"nama" binding:"required"`
-	Email string `json:"email" binding:"required,email"`
+	Stok  int    `json:"stok" binding:"required,min=1"`
+	Harga int    `json:"harga" binding:"required,min=1000"`
 }
 
 func main() {
 	r := gin.Default()
 
-	r.POST("/register", func(c *gin.Context) {
-		var req RegisterRequest
+	r.POST("/produk/tambah", func(c *gin.Context) {
+		var req ProductRequest
 
 		if err := c.ShouldBindJSON(&req); err != nil {
 			c.JSON(400, gin.H{"error": err.Error()})
@@ -20,7 +21,7 @@ func main() {
 		}
 
 		c.JSON(200, gin.H{
-			"pesan": "register berhasil",
+			"pesan": "produk ditambahkan",
 			"data":  req,
 		})
 	})
